d11c2aa4a7d4679b104d05251b7ccd65e95376e0
Michael Rollins <michael.rollins@gmail.com>
2023-07-21 10:05:44 -0400

    Initial Commit

---
+++ image-spinner/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  image-spinner
+//
+//  Created by Michael Rollins on 7/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ image-spinner/image_spinnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  image_spinnerApp.swift
+//  image-spinner
+//
+//  Created by Michael Rollins on 7/21/23.
+//
+
+import SwiftUI
+
+@main
+struct image_spinnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
