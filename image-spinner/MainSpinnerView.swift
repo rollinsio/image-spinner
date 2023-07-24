2ad1d23754e980046361315531a4d01ba7088be5
Michael Rollins <michael.rollins@gmail.com>
2023-07-24 08:42:21 -0400

    Adds swiping behavior

--- image-spinner/MainSpinnerView.swift
+++ image-spinner/MainSpinnerView.swift
@@ -11,25 +11,41 @@ struct MainSpinnerView: View {
     @ObservedObject var viewModel = PulseViewModel(imageName: "squareLogo")
     @State var showSheet: Bool = false
     
+    let views = [Pulse(viewModel: PulseViewModel(imageName: "squareLogo")),
+                 Pulse(viewModel: PulseViewModel(imageName: "dragon")),
+                 Pulse(viewModel: PulseViewModel(imageName: "dragonFull"))
+    ]
+    
     var body: some View {
         
-        GeometryReader { geometry in
-            VStack {
-                Pulse(viewModel: viewModel)
-                    .frame(width: geometry.size.width, height: geometry.size.height * 0.9)
-                
-                Button(action: {
-                    self.showSheet.toggle()
-                }) {
-                    Text("Show Code")
+        VStack {
+            TabView {
+                ForEach(0..<3) { i in
+                    views[i]
+                        .padding()
                 }
-                .frame(width: geometry.size.width * 0.9, height: geometry.size.height * 0.1)
             }
-            .sheet(isPresented: $showSheet) {
-                Text(viewModel.getCode())
-                    .font(.system(size: 10, weight: .bold, design: .monospaced))
-            }
+            .tabViewStyle(PageTabViewStyle())
+            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
         }
+        
+//        GeometryReader { geometry in
+//            VStack {
+//                Pulse(viewModel: viewModel)
+//                    .frame(width: geometry.size.width, height: geometry.size.height * 0.9)
+//
+//                Button(action: {
+//                    self.showSheet.toggle()
+//                }) {
+//                    Text("Show Code")
+//                }
+//                .frame(width: geometry.size.width * 0.9, height: geometry.size.height * 0.1)
+//            }
+//            .sheet(isPresented: $showSheet) {
+//                Text(viewModel.getCode())
+//                    .font(.system(size: 10, weight: .bold, design: .monospaced))
+//            }
+//        }
     }
         
 
